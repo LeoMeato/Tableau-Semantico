f2c99dc13ee1d1a47414b5407a9b3a18df8dd2cd
Leonardo Meato <leomeato@gmail.com>
2024-06-05 20:16:58 -0300

    Update tableau.hs

--- tableau.hs
+++ tableau.hs
@@ -1,19 +1,58 @@
 data Tree = Leaf {content :: [String]} | Node {content :: [String], fstchild :: Tree, sndchild :: Tree} deriving (Show)
 
-{-
 tableau :: String -> Tree
-tableau entrada = montaTableau (Node [entrada] (Leaf []) (Leaf []))
+tableau entrada = montaTableau (Leaf [entrada])
+
+------------------------------------------------------- Montagem da Árvore -------------------------------------------------------------------
+
+-- Ainda falta jogar as conclusões pras folhas
 
 montaTableau :: Tree -> Tree
 montaTableau (Node (h:t) fst snd) = aplicaRegra (detectaPadrao h) h (Node t fst snd)
+montaTableau (Node [] (Node (h1:t1) fst1 snd1) (Node (h2:t2) fst2 snd2)) = (Node [] (aplicaRegra (detectaPadrao h1) h1 (Node t1 fst1 snd1)) (aplicaRegra (detectaPadrao h2) h2 (Node t2 fst2 snd2)))
+montaTableau (Leaf []) = (Leaf [])
+montaTableau (Leaf (h:t)) = aplicaRegra (detectaPadrao h) h (Leaf t)
 
 aplicaRegra :: (Int, [String]) -> String -> Tree -> Tree
 
-aplicaRegra (1, [formula1, formula2]) h (Node t fst snd) = Node(junta h (Node t++[formula1]++[formula2] fst snd))
+aplicaRegra (1, [formula1, formula2]) h (Node t fst snd) = junta h (montaTableau (Node (t++[formula1]++[formula2]) fst snd))
+aplicaRegra (1, [formula1, formula2]) h (Leaf t) = junta h (montaTableau (Leaf (t++[formula1]++[formula2])))
+
+aplicaRegra (2, [formula1, formula2]) h (Node t fst snd) = junta h (Node t (Leaf ((content fst)++[formula1])) (Leaf ((content snd)++[formula2])))
+aplicaRegra (2, [formula1, formula2]) h (Leaf t) = junta h (Node t (Leaf [formula1]) (Leaf [formula2]))
+
+aplicaRegra (3, [formula1, formula2]) h (Node t fst snd) = junta h (Node t (Leaf ((content fst)++["(~, "++formula1++")"])) (Leaf ((content snd)++[formula2])))
+aplicaRegra (3, [formula1, formula2]) h (Leaf t) = junta h (Node t (Leaf ["(~, "++formula1++")"]) (Leaf [formula2]))
+
+aplicaRegra (4, [formula1, formula2]) h (Node t fst snd) = junta h (Node t (Leaf ((content fst)++["(^, "++formula1++", "++formula2++")"])) (Leaf ((content snd)++["(^, (~, "++formula1++"), (~, "++formula2++"))"])))
+aplicaRegra (4, [formula1, formula2]) h (Leaf t) = junta h (Node t (Leaf ["(^, "++formula1++", "++formula2++")"]) (Leaf ["(^, (~, "++formula1++"), (~, "++formula2++"))"]))
+
+aplicaRegra (5, [formula]) h (Node t fst snd) = junta h (montaTableau (Node (t++[formula]) fst snd))
+aplicaRegra (5, [formula]) h (Leaf t) = junta h (montaTableau (Leaf (t++[formula])))
+
+aplicaRegra (6, [formula1, formula2]) h (Node t fst snd) = junta h (Node t (Leaf ((content fst)++["(~, "++formula1++")"])) (Leaf ((content snd)++["(~, "++formula2++")"])))
+aplicaRegra (6, [formula1, formula2]) h (Leaf t) = junta h (Node t (Leaf ["(~, "++formula1++")"]) (Leaf ["(~, "++formula2++")"]))
+
+aplicaRegra (7, [formula1, formula2]) h (Node t fst snd) = junta h (montaTableau (Node (t++["(~, "++formula1++")"]++["(~, "++formula2++")"]) fst snd))
+aplicaRegra (7, [formula1, formula2]) h (Leaf t) = junta h (montaTableau (Leaf (t++["(~, "++formula1++")"]++["(~, "++formula2++")"])))
+
+aplicaRegra (8, [formula1, formula2]) h (Node t fst snd) = junta h (montaTableau (Node (t++[formula1]++["(~, "++formula2++")"]) fst snd))
+aplicaRegra (8, [formula1, formula2]) h (Leaf t) = junta h (montaTableau (Leaf (t++[formula1]++["(~, "++formula2++")"])))
+
+aplicaRegra (9, [formula1, formula2]) h (Node t fst snd) = junta h (Node t (Leaf ((content fst)++["(^, (~, "++formula1++"), "++formula2++")"])) (Leaf ((content snd)++["(^, (~, "++formula1++"), "++formula2++")"])))
+aplicaRegra (9, [formula1, formula2]) h (Leaf t) = junta h (Node t (Leaf ["(^, (~, "++formula1++"), "++formula2++")"]) (Leaf ["(^, (~, "++formula1++"), "++formula2++")"]))
+
+aplicaRegra (10, [formula]) h (Node t fst snd) = junta h (montaTableau (Node t fst snd))
+aplicaRegra (10, [formula]) h (Leaf t) = junta h (montaTableau (Leaf t))
+
+aplicaRegra (11, [formula]) h (Node t fst snd) = junta h (montaTableau (Node t fst snd))
+aplicaRegra (11, [formula]) h (Leaf t) = junta h (montaTableau (Leaf t))
 
 junta :: String -> Tree -> Tree
 junta h (Node t fst snd) = (Node (h:t) fst snd)
--}
+junta h (Leaf t) = (Leaf (h:t))
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
 
 -------------------------------------- Detecção de Padrão -----------------------------------------------
 
